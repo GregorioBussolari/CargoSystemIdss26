dda2350a58825c55527226fa5cdcaa72855b04e9
Gregorio Bussolari <gregoriobusso@gmail.com>
2026-07-13 17:00:35 +0200

    Updtate sprint 0

--- sprint1/CargoSystem26/cargosystem.pl
+++ sprint1/CargoSystem26/cargosystem.pl
@@ -15,6 +15,7 @@ reply( loadRejected, loadRejected(X) ).  %%for loadRequest
 dispatch( buttonPressed, buttonPressed(X) ). %Richiesta di carico da parte di un utente
 event( timeOut, timeOut(X) ). %Scadenza regola dei 30 secondi
 event( loadEnded, loadEnded(X) ). %Fine procedura di carico
+dispatch( updateHoldState, updateHoldState(FREESLOT) ). %Fine procedura di carico, comunica anche il numero di slot rimanenti
 event( endOOS, endOOS(X) ). %Fine stato Out Of Service
 event( containerDetected, containerDetected(X) ). %Emesso se D < DFREE/2 per 3s
 event( sensorAlarm, sensorAlarm(X) ). %Emesso se D > DFREE per 3s
