9b2bd7d24282887ffbcacca36ddab94b8b141767
Gregorio Bussolari <gregoriobusso@gmail.com>
2026-07-13 17:15:15 +0200

    primo refactor sprint 1

--- sprint1/CargoSystem26/cargosystem.pl
+++ sprint1/CargoSystem26/cargosystem.pl
@@ -16,9 +16,11 @@ dispatch( buttonPressed, buttonPressed(X) ). %Richiesta di carico da parte di un
 event( timeOut, timeOut(X) ). %Scadenza regola dei 30 secondi
 event( loadEnded, loadEnded(X) ). %Fine procedura di carico
 dispatch( updateHoldState, updateHoldState(FREESLOT) ). %Fine procedura di carico, comunica anche il numero di slot rimanenti
+event( startOOS, startOOS(X) ). %Inizio stato Out Of Service
 event( endOOS, endOOS(X) ). %Fine stato Out Of Service
 event( containerDetected, containerDetected(X) ). %Emesso se D < DFREE/2 per 3s
 event( sensorAlarm, sensorAlarm(X) ). %Emesso se D > DFREE per 3s
+event( sensorRestored, sensorRestored(X) ). %Emesso se D < DFREE per 3s
 dispatch( startBlink, startBlink(X) ). %Gestione accensione blink LED
 dispatch( stopBlink, stopBlink(X) ). %Gestione spegnimento blink LED
 event( startSensorRecording, startSensortRecording(X) ). %Inizio fase di rilevazione dei 30 secondi
